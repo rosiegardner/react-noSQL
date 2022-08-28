a0cb7943760a35130f6f038b724cda112da8353a
Rosie Gardner <rosiegardner78@gmail.com>
2022-08-28 16:07:53 -0700

    remove v4 uuid

--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { v4 } from 'uuid';
+// import { v4 } from 'uuid';
 import PropTypes from "prop-types"; 
 import ReusableForm from "./ReusableForm";
 
@@ -7,7 +7,12 @@ function NewTicketForm(props){
 
   function handleNewTicketFormSubmission(event) {
     event.preventDefault();
-    props.onNewTicketCreation({names: event.target.names.value, location: event.target.location.value, issue: event.target.issue.value, id: v4()});
+    props.onNewTicketCreation({
+      names: event.target.names.value,
+      location: event.target.location.value,
+      issue: event.target.issue.value, 
+      // id: v4()
+    });
   }
 
   return (
